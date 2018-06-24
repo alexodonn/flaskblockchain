165290e150cff7b3267e193808ed14f653c7f401
alexodonn <alexodonn@gmail.com>
2018-06-24 15:03:36 -0300

    preparing tocreate API

--- model/Blockchain.py
+++ model/Blockchain.py
@@ -12,6 +12,10 @@ class Blockchain:
         self.chain = []
         self.genesis()
 
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     # generates a genesis block and appends it to the chain
     def genesis(self):
         genesis_block = Block(0, [], time.time(), "0")
@@ -29,6 +33,41 @@ class Blockchain:
 
         return hash
 
-    @property
-    def last_block(self):
-        return self.chain[-1]
+    # check if a block_hash is valid and satisfies our difficulty criteria
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.hasher())
+
+    # adds a block to the chain after verification
+    def add_block(self, block, proof):
+        last_hash = self.last_block.hash
+
+        if last_hash != block.last_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def add_new_transaction(self, transaction):
+        self.uncomfirmed_transactions.append(transaction)
+
+    # serves as an interface to add the per transactions to the blockchain
+    def mine(self):
+        if not self.uncomfirmed_transactions:
+            return False
+
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions=self.uncomfirmed_transactions,
+                          timestamp=time.time(),
+                          last_hash=last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.uncomfirmed_transactions = [0]
+        return new_block.index
